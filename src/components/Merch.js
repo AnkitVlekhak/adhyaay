c387f1ac1627f5d2dcb1dcdff8a2d566139f8615
unknown <priyanshusgvm@gmail.com>
2024-01-07 12:39:18 +0530

    Merchandise

--- src/components/Merch.js
+++ src/components/Merch.js
@@ -1,17 +1,38 @@
 /* eslint-disable jsx-a11y/alt-text */
-import merch from "../assets/merch.jpg";
 
+import Card from "./Card";
+import "./Merch.scss";
 const Merch = () => {
+
+  
+    const cardsData = [
+      {
+        name: "Adhyaay T-shirt",
+        detail: "A high-top version of the classic T-shirt.",
+      },
+      {
+        name: "Adhyaay Second T-shirt",
+        detail: "Description for 2nd T-shirt. Good T-shirt",
+      },
+      {
+        name: "Adhyaay Third T-shirt",
+        detail: "Details for the 3rd T-shirt. Good T-shirt",
+      },
+    ];
+  
   return (
-    <div
+    <div className="mine"
       style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "black",
+        
       }}
     >
-      <img className="img" src={merch} style={{ marginTop: 100 }}></img>
+     <div className="t-box"> {cardsData.map((data, index) => (
+      <Card key={index} name={data.name} detail={data.detail} />
+    ))}</div>
     </div>
   );
 };
