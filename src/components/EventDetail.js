4482dee8dc94d9a40d9f9a27d22c34a1bd44e77a
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-01-11 21:39:42 +0530

    Update EventDetail.js

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -21,7 +21,7 @@ const EventDetail = () => {
     >
     <img src={img} className="eventImg pt-20 w-1/3" />
     <div className="w-2/3 pt-[6vw] flex flex-col items-center">
-      <h1 style={{ color: "#dbba89", marginTop: 20 }}>{name}</h1>
+      <h1 style={{ color: "#dbba89", marginTop: 20 }} className="text-center text-6xl font-semibold font-serif tracking-wide">{name}</h1>
       <p
         className="text-2xl text-[#dbba89] mt-10 w-[85%]"
         style={{
